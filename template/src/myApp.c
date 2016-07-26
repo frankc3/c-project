dc879e0eb63449331192ab1d5545cee6e0837ad5
Frank Cabuy <frank.cabuy@gmail.com>
2016-07-26 23:13:58 +0200

    doxyfile added

--- template/src/myApp.c
+++ template/src/myApp.c
@@ -13,10 +13,20 @@
 #include "myApp.h"
 #define  DEBUG  //print debug messages
 #include "mydebug.h"
+/** \How to generate docs with doxygen \n
+ *  doxygen-g cerate Doxyfile (=config file) \n
+ *  change in config file \n
+ *    INPUT                  = src/main.c src/myApp.h src/myApp.c \n
+ *    EXTRACT_ALL            = YES \n
+ *    OUTPUT_DIRECTORY       = docs \n
+ * doxygen Doxyfile generates documentation \n
+ */
+
 
 /**
-* Docygen documentation
+* Here comes the function description for myfunction
 */
+
 int myfunction (int * nbr)
 {
     LOG_INFO("Log info message");
